fab3a96439f55fd3a0717e939f2bdd23f3a809ce
Brice 4Mole <kaszlukb@gmail.com>
2020-05-11 17:43:12 +0200

    ajoute un truc

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p> bienvenue sur le site web de moi</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
